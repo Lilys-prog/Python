bb197285aaaeba259510565e7b9e19ede3d4da5b
Lily Silva <lilys.prog@gmail.com>
2022-10-24 18:07:06 -0300

    tabuada SEM estrutura de repetição

---
+++ Aula 07/ex015.py
@@ -0,0 +1,13 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
+num = int(input('Digite um número: '))
+tab1 = num * 1
+tab2 = num * 2
+tab3 = num * 3
+tab4 = num * 4
+tab5 = num * 5
+tab6 = num * 6
+tab7 = num * 7
+tab8 = num * 8
+tab9 = num * 9
+print('A tabuada de {} é\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}'.format(num, tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8, tab9))
+
