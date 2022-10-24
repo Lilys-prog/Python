c40c2ee78ebd26f298838794444f62d273fcfe58
Lily Silva <lilys.prog@gmail.com>
2022-10-24 17:47:27 -0300

    exercício de média aritmética

--- Aula 07/ex013.py
+++ Aula 07/ex013.py
@@ -1,1 +1,5 @@
 # Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+print('O aluno teve as notas {} e {}, o que resultou em uma média de {}.'.format(nota1, nota2, media))
