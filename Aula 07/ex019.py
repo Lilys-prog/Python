f40ce522b4b41781dd2da15a4d51f6456e6b3492
Lily Silva <lilys.prog@gmail.com>
2022-10-24 18:46:43 -0300

    aumentar 15% do salário (quem dera)

---
+++ Aula 07/ex019.py
@@ -0,0 +1,4 @@
+# Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento
+sal1 = float(input('Quanto você recebe por mês? '))
+salN = sal1 + sal1*0.15
+print('Parabéns! Seu novo salário é de {} reais!'.format(salN))
