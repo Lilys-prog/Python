6509b9350fb4f8f800953425bb6774df6b0a17c5
Lily Silva <lilys.prog@gmail.com>
2022-11-13 22:56:54 -0300

    aula sobre módulos internos e externos

--- Aula 08/ex022.py
+++ Aula 08/ex022.py
@@ -1,1 +1,7 @@
-# começando parte prática da aula
+# começando parte prática da aula 08
+from math import sqrt, floor
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print(f'A raiz quadrada de {num} é igual a {raiz:.2f}.') # formatação para casas decimais {variável:.2f}
+# outra forma: {num}, {floor(raiz)}
+# no próprio site do python é possível ver a lista com as dezenas de bibliotecas que podem ser importadas, em docs
---
+++ Aula 08/ex023.py
@@ -0,0 +1,9 @@
+# usando a biblioteca matemática de gerar números aleatórios
+import random
+num = random.random()
+print(num)
+# o método random da classe random gera um número aleatório entre [0, 1[
+# para números inteiros usa-se o randint(min, max)
+nInt = random.randint(1, 100)
+print(nInt)
+
