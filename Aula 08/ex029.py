1a16ce896815307d234ec5bc130562115b9d5604
Lily Silva <lilys.prog@gmail.com>
2022-12-22 15:46:28 -0300

    tocando mp3 pelo python

--- Aula 08/ex029.py
+++ Aula 08/ex029.py
@@ -1,11 +1,24 @@
 # Faça um programa em Python que abra e reproduza o áudio de um arquivo mp3.
 # o pygame é um módulo para construção de jogos. Como jogos usam música, ele também pode ser usado neste exercício
-import pygame
-pygame.mixer.init()
-pygame.init()
-pygame.mixer.music.load('hotel.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()
+from pygame import mixer
+mixer.init()
+mixer.music.load('hotel.mp3')
+mixer.music.set_volume(0.7)
+mixer.music.play()
+while True:
+    print("Press 'p' to pause, 'r' to resume")
+    print("Press 'e' to exit the program")
+    query = input("  ")
+    if query == 'p':
+        # Pausing the musice
+        mixer.music.pause()
+    elif query == 'r':
+        # Resuming the music
+        mixer.music.unpause()
+    elif query == 'e':
+        # Stop the mixer
+        mixer.music.stop()
+        break
 
 
 
